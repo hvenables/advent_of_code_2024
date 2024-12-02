e31f74e098896a5f106c7aff0bdc50f5acf2f692
Harry Venables <hvenables89@gmail.com>
2024-12-02 14:59:27 +0000

    Update to the fetcher

--- day_generator.rb
+++ day_generator.rb
@@ -7,7 +7,12 @@ dir_name = "day_#{day_num}"
 raise "Day already exists" if File.directory?(dir_name)
 
 system("mkdir #{dir_name}")
-system("touch #{dir_name}/day_#{day_num}.rb")
+
+content = <<~CONTENT
+  inputs = File.read("input.txt")
+CONTENT
+
+File.write("#{dir_name}/day_#{day_num}.rb", content)
 
 url = URI.parse("https://adventofcode.com/2024/day/#{day_num}/input")
 session_cookie = ENV["AOC_SESSION_COOKIE"]
@@ -21,9 +26,7 @@ request['Cookie'] = "session=#{session_cookie}"
 response = http.request(request)
 
 if response.is_a?(Net::HTTPSuccess)
-  File.open("#{dir_name}/input.txt", 'w') do |file|
-    file.write(response.body)
-  end
+  File.write("#{dir_name}/input.txt", response.body)
 else
   raise "HTTP Error: #{response.code} - #{response.message}"
 end
